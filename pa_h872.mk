d8e4a8d8a59058f445f280f7f186f49d5e621eb8
dom133 <dom133_pl@protonmail.com>
2020-06-04 01:04:50 +0200

    h872: PA Bringup

--- pa_h872.mk
+++ pa_h872.mk
@@ -16,13 +16,16 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Include Paranoid Android common configuration
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Inherit from h872 device
 $(call inherit-product, device/lge/h872/device.mk)
+
+include device/qcom/common/common.mk
+include vendor/pa/config/common_full_phone.mk
 
 # Security patch level
 PLATFORM_SECURITY_PATCH_OVERRIDE := 2018-10-01
@@ -31,7 +34,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH_OVERRIDE)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := h872
-PRODUCT_NAME := lineage_h872
+PRODUCT_NAME := pa_h872
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H872
 PRODUCT_MANUFACTURER := LGE
